ad4155b5e9c466f117bb7884a501dc90b75e6a4e
msa <msai.sadeghi@gmail.com>
2023-10-05 21:58:35 +0330

    s

--- s1.py
+++ s1.py
@@ -1,1 +1,50 @@
-print("hello")
+# print("hello")
+
+# x = 12.1
+# print(x)
+# print(type(x))
+
+# a = 12
+# b = 13
+
+# print(a + b)
+# print(a - b)
+# print(a * b)
+# print(a / b)
+# print(a // b)
+# print(a % b)
+
+# name = 'yasna'
+# print(type(name))
+# name = "yasna"
+# print(type(name))
+
+# message = 'hello "yasna"'
+# print(message)
+
+# message = "hello \"yasna\""
+# print(message)
+
+
+# name = "yasna"
+# print(name[0])
+# print(name[1])
+# print(name[2])
+# print(name[3])
+# print(name[4])
+
+# name = input("Enter a name: ")
+# print("hello",name)
+
+
+# a = int(input("enter a number: "))
+# b = int(input("enter a number: "))
+# result = a + b
+# print(result)
+name = input("enter your name: ")
+family = input("enter your family: ")
+print("hello", name, family)
+
+# TODO
+
+# با کمک + کار بالا را انجام بده
