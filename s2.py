fdf07fef3e4995be8ad50cee0f8f8d11b35439dc
msa <msai.sadeghi@gmail.com>
2023-10-12 21:58:38 +0330

    s

---
+++ s2.py
@@ -0,0 +1,48 @@
+# a = int(input("enter a number: "))
+# b = int(input("enter a number: "))
+# result = a + b
+# print(result)
+
+
+# name = input("enter your name: ")
+# family = input("enter your family: ")
+# print("hello", name, family)
+
+# print("hello "+ name+ " " +family)
+
+
+# name = input("enter a name: ")
+# age = input("enter an age: ")
+# message = name + " is " + age
+# message = f"{name} is {age}"
+# print(message)
+
+
+# x = 1
+
+# x = "yasna"
+# print(x)
+
+
+course1 = "python"
+print(type(course1))
+course2 = 'python'
+print(type(course2))
+print(len(course1))
+
+
+message = 'yasna said "what\'s the difference python and java"'
+print(message)
+
+# TODO
+"""
+برنامه ای بنویس که نام یک دانش آموز از ورودی دریافت کنه
+تایپش رو پرینت کنه
+طولش رو پرینت کنه
+همینطور اسم دو دوره آموزشی مورد علاقه اش رو ازش بپرسه
+و متن زیر رو پرینت کنه
+
+sara balalallal likes "python" and "s".
+
+
+"""
