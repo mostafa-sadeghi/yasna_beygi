5f818e491e8e20db581cb1a4f433458aaf8c1384
msa <msai.sadeghi@gmail.com>
2023-10-19 21:58:15 +0330

    s

---
+++ s3.py
@@ -0,0 +1,47 @@
+# a = 12
+# b = 13
+
+# if a != b: #   >        <       ==      !=      >=      <=
+#     print("a is not equal to b")
+#     print("blallalalal")
+# print("outside")
+
+
+# x = int(input('enter first number: '))
+# y = int(input('enter second number: '))
+# z = int(input('enter third number: '))
+
+# if x > y and x > z:
+#     print(f'{x} is greater than {y} and {z}')
+# elif y > x and y > z:
+#     print(f'{y} is greater than {x} and {z}')
+# else:
+#     print(f'{z} is greater than {x} and {y}')
+# TODO
+"""
+برنامه ای بنویسی که نمرات سه درس یک دانش آموز را از ورودی دریافت نماید
+معدل او را محاسبه کن
+اگر معدلش از 90 بیشتر بود
+A
+را پرینت کن
+اگر معدلش از 80 بیشتر بود
+B
+را پرینت کن
+اگر معدلش از 70 بیشتر بود
+C
+را پرینت کن
+اگر معدلش از 60 بیشتر بود
+D
+را پرینت کن
+در غیر اینصورت 
+E
+را پرینت کن
+"""
+
+username = input("enter the username: ")
+if username == "root" or username == "sara" or username == "admin":
+    print(f'{username} is valid')
+    print("login success")
+else:
+    print(f'{username} is not valid')
+    print("login was'nt successful")
